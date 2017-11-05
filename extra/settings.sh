0d13fbafa55f12b6eb36d71484b252c3bce132a7
lnmunhoz <ln.munhoz@gmail.com>
2017-11-06 03:16:15 +0700

    Update settings script to get dynamic environment

--- extra/settings.sh
+++ extra/settings.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "-----> Updating METEOR_SETTINGS"
-PATH_TO_SETTINGS="$APP_CHECKOUT_DIR"/settings/staging.json
+PATH_TO_SETTINGS="$APP_CHECKOUT_DIR"/settings/"$NODE_ENV".json
 export METEOR_SETTINGS="$(cat ${PATH_TO_SETTINGS})"
