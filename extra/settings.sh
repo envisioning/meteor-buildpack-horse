543b5f194a603d4c6c3830f201e89a35e1ff994a
lnmunhoz <ln.munhoz@gmail.com>
2018-01-26 15:15:23 -0200

    Echo environment when updating METEOR_SETTINGS

--- extra/settings.sh
+++ extra/settings.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "-----> Updating METEOR_SETTINGS"
+echo "-----> Updating METEOR_SETTINGS for $NODE_ENV environment"
 PATH_TO_SETTINGS="$APP_CHECKOUT_DIR"/settings/"$NODE_ENV".json
 export METEOR_SETTINGS="$(cat ${PATH_TO_SETTINGS})"
